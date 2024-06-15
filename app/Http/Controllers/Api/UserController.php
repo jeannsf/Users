ad68b62bc28213de65009e718ee4673d0234384f
Jean Freitas <jeannsfreitas@gmail.com>
2024-06-15 20:35:18 -0300

    feat: add endpoint to create a new user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -27,7 +29,6 @@ class UserController extends Controller
               ], 200);
     }
 
-
   /**
      * Exibe os detalhes de um usuário específico. 
      * 
@@ -41,5 +42,48 @@ class UserController extends Controller
             'status' => true,
             'user' => $user,
         ], 200);
+    }
+
+
+    /**
+     * Cria novo usuário com os dados fornecidos na requisição.
+     * 
+     * @param  \App\Http\Requests\UserRequest  $request O objeto de requisição contendo os dados do usuário a ser criado.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(UserRequest $request): JsonResponse
+    {
+        // Iniciar a transação
+        DB::beginTransaction();
+
+        try {
+
+            // Cadastrar usuário no banco de dados
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password,
+            ]);
+
+            // Operação é concluída com êxito
+            DB::commit();
+
+            // Retorna os dados do usuário criado e uma mensagem de sucesso com status 201
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'message' => "Usuário cadastrado com sucesso!",
+            ], 201);
+        } catch (Exception $e) {
+
+            // Operação não é concluída com êxito
+            DB::rollBack();
+
+            // Retorna uma mensagem de erro com status 400
+            return response()->json([
+                'status' => false,
+                'message' => "Usuário não cadastrado!",
+            ], 400);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/users", [UserController::class, 'index']); // GET -> http://127.0.0.1:8000/api/users?page=1
 Route::get("/users/{user}", [UserController::class,"show"]); // GET -> http://127.0.0.1:8000/api/users/1
+Route::post("/users", [UserController::class,"store"]); // POST -> http://127.0.0.1:8000/api/users
