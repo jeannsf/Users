96b22b6d4841253ca29e52adf63b02fffdf7389f
Jean Freitas <jeannsfreitas@gmail.com>
2024-06-15 20:00:34 -0300

    feat: add endpoint to display specific user details

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
     /**
      * Retorna uma lista paginada dde usuários.
      * 
-     * Este Método recupera uma lista paginada de usuários do banco de daods
+     * Este Método recupera uma lista paginada de usuários do banco de dados
      * e a retorna como uma resposta JSON.
      * 
      * @return \Illuminate\Http\JsonResponse
@@ -25,5 +25,21 @@ class UserController extends Controller
                   'status' => true,
                   'users' => $users,
               ], 200);
+    }
+
+
+  /**
+     * Exibe os detalhes de um usuário específico. 
+     * 
+     * Este Método retorna os detalhes de um usuário específico em formato JSON.
+     * 
+     * @param \App\Models\User $user o objeto do usuário a ser exibido
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(User $user):JsonResponse {
+        return response()->json([
+            'status' => true,
+            'user' => $user,
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,4 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/users", [UserController::class, 'index']); // GET -> http://127.0.0.1:8000/api/users?page=1
+Route::get("/users/{user}", [UserController::class,"show"]); // GET -> http://127.0.0.1:8000/api/users/1
